da3a5c167851108f88bb91aa8f519081598e5c24
melis-a <rampersaudmelisa@gmail.com>
2023-03-02 03:41:15 +0000

    array

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -15,7 +15,11 @@ public class Lab {
      * @return the length of arr.
      */
     public int getLengthOfArray(int[] arr){
-        return 0;
+        
+       // arr = new int[10];
+       // System.out.println(arr.length);
+       return arr.length;
+       
     }
 
 
@@ -27,26 +31,46 @@ public class Lab {
      * @return the element at index n of arr.
      */
     public int getNthElementOfArray(int[] arr, int n){
-        return 0;
-    }
-
+     // for (int i = 0; i < arr.length; i++){
+     //   arr = new int[n];
+      
+      return (arr[n]);
+      }
+      
+  
     /**
      *
      * @param arr the array we're getting data from.
      * @param n the index (number) of the element we're looking to modify.
      *          Indexes in arrays start counting from zero.
      * @param val the value that we will assign to a spot in the array.
+     * @return 
      * @return nothing. Because of pass-by-reference, any change to the array will be reflected across our java program.
      */
     public void setNthElementOfArray(int[] arr, int n, int val){
-
+     
+      for(int i = 0; i < arr.length; i++){
+        System.out.println(arr[n] = val);
+       
+      // return (arr[n] = val);
+       
     }
+  }
+      
+  
+      
+    
 
     /**
      * @param n the size of the array we wish to create
      * @return a new int array that can contain n elements.
      */
     public int[] returnNewArraySizeN(int n){
-        return null;
+       // int [] arr;
+       // arr = new int[n];
+        return new int [n];
+           // return null;
+        
     }
-}
+  
+  }
